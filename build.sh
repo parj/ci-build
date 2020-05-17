bc672e0fec7c99ced0c591c78fbb082a850594b6
Parjanya Mudunuri <mudupa@mammoth.local>
2020-05-17 16:21:09 +0100

    Updated jib command to push

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 MAVEN_SETTINGS="$DIR/resources/settings.xml"
-DOCKER_BUILD_PARAMS="jib:dockerBuild"
+DOCKER_BUILD_PARAMS="jib:build"
 DRY_RUN=false
 
 function main() {
